9bb66cb5d687ed54164568dca2b9c5dd00f1b15f
ShaliniAutomation <121995783+ShaliniAutomation@users.noreply.github.com>
2023-02-20 23:56:35 -0500

    Add files via upload

---
+++ RESTAssuredAutomation/src/test/java/tests/PutPatchDeleteExamples.java
@@ -0,0 +1,78 @@
+package tests;
+
+import static io.restassured.RestAssured.*;
+
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.json.simple.JSONObject;
+import org.testng.annotations.Test;
+
+import io.restassured.http.ContentType;
+
+public class PutPatchDeleteExamples {
+	@Test
+	public void testPost() {
+		
+	
+		JSONObject request = new JSONObject();
+		
+		request.put("name", "Shalini");
+		request.put("job", "Teacher");
+	System.out.println(request.toJSONString());
+	
+	baseURI="https://reqres.in/api";
+	
+	given().
+	header("Content-Type", "application/json").
+	 contentType(ContentType.JSON).
+	 accept(ContentType.JSON).
+	 body(request.toJSONString()).
+	when().
+	 post("/users/2").
+	then().
+	statusCode(201).
+	log().all();
+	}
+
+	@Test
+	public void testPatch() {
+		
+	
+		JSONObject request = new JSONObject();
+		
+		request.put("name", "Shalini");
+		request.put("job", "Teacher");
+		
+	System.out.println(request.toJSONString());
+	
+	baseURI="https://reqres.in";
+	
+	given().
+	header("Content-Type", "application/json").
+	 contentType(ContentType.JSON).
+	 accept(ContentType.JSON).
+	 body(request.toJSONString()).
+	when().
+	 patch("/api/users/2").
+	then().
+	statusCode(200).
+	log().all();
+	}
+	
+	@Test
+	public void testDelete() {
+		
+	
+	baseURI="https://reqres.in";
+	
+	
+	
+    when().
+	 delete("/api/users/2").
+	then().
+	statusCode(204).
+	log().all();
+	}
+}
